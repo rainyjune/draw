3d60236afe43f6d6acde36720ad7b77589a814d8
rainyjune <rainyjune@live.cn>
2015-05-06 12:27:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 window.addEventListener("load", initApp, false);
 
+var ongoingTouches = new Array();
+
 function initApp() {
   addEventListeners();
+  startup();
 }
 
 function addEventListeners() {
-  document.getElementById("initButton").addEventListener("click", startup, false);
+  //document.getElementById("initButton").addEventListener("click", startup, false);
 }
 
 function startup() {
@@ -18,20 +21,48 @@ function startup() {
   log("initialized.");
 }
 
-function handleStart() {
+function handleStart(event) {
+  event.preventDefault();
+  log("touchstart.");
+  var el = document.querySelector("#canvas");
+  var ctx = el.getContext("2d");
+  var touches = event.changedTouches;
   
+  for (var i = 0, len = touches.length; i < len; i++) {
+    log("touchstart:" + i + "...");
+    var thisTouch = touches[i];
+    ongoingTouches.push(Object.create(thisTouch));
+    var color = colorForTouch(thisTouch);
+    ctx.beginPath();
+    ctx.arc(thisTouch.pageX, thisTouch.pageY, 4, 0, 2 * Math.PI, false);
+    ctx.fillStyle = color;
+    ctx.fill();
+    log("touchstart:" + i + ".");
+  }
 }
 
-function handleMove() {
-  
+function handleMove(event) {
+  //console.log("touchmove event:", event);
 }
 
-function handleEnd() {
-  
+function handleEnd(event) {
+  //console.log("touchend event:", event);
 }
 
-function handleCancel() {
-  
+function handleCancel(event) {
+  //console.log("touchcancel event:", event);
+}
+
+function colorForTouch(touch) {
+  var r = touch.identifier % 16;
+  var g = Math.floor(touch.identifier / 3) % 16;
+  var b = Math.floor(touch.identifier / 7) % 16;
+  r = r.toString(16); // make it a hex digit
+  g = g.toString(16); // make it a hex digit
+  b = b.toString(16); // make it a hex digit
+  var color = "#" + r + g + b;
+  log("color for touch with identifier " + touch.identifier + " = " + color);
+  return color;
 }
 
 function log(msg) {
